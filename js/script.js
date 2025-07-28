3977c19a51deb821ba59d638135007991ff845a9
Astral-Viscount <md.mahatabmahimn@gmail.com>
2025-07-28 22:57:21 +1200

    adding commands

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.getElementById("body");
+    const prompt = document.getElementById("prompt");
+    const cursor = document.getElementById("cursor");
+
+    const commands = {
+    help: `Available commands:<br> - about<br> - skills<br> - projects<br> - contact<br> - clear`,
+    about: `Hi, I'm Mahi Mahatab. I'm a high school student and aspiring software engineer with interests in computer engineering, AI, and electronics.`,
+    skills: `Languages: HTML, CSS, JavaScript, Python, Lua<br>Tools: GitHub, VS Code, Tinkercad, PROS, Fritzing, KiCad`,
+    projects: `1. Virtual Assistant (Python)<br>2. Logic Gate Circuits (Tinkercad)<br>3. Portfolio Website<br>4. AI Snake Game<br>4. 4-bit Calculator`,
+    contact: `Email:<a href="mailto:md.mahatabmahimn@gmail.com" target="_blank">md.mahatabmahimn@gmail.com</a><br>GitHub: <a href="https://github.com/Astral-Viscount" target="_blank">Astral-Viscount</a>`,
+    clear: 'clear'
+    };
+
+    
+});
